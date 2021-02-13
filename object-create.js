f821b541f68b7c960a0b8986ccaebff5d1e2f230
Dave Lilleystone <davelilleystone@gmail.com>
2021-02-13 19:52:33 +0000

    initial commit

---
+++ object-create.js
@@ -0,0 +1,17 @@
+// Object.create creates a new object, with the first argument being thhe prototype of the new object.
+
+
+const parent = {
+    parent: true,
+    printMessage(){
+        console.log(`I am ${this.parent ? 'the' : 'not the'} parent`)
+    }
+    
+}
+
+const child = Object.create(parent, {parent: {value:false}})
+
+
+
+parent.printMessage()
+child.printMessage()
